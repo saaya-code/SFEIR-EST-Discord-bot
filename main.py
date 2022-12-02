cf852674414507aa9cadad19e920cce5e9316872
Younes-ch <younes.jmell10@gmail.com>
2022-12-02 01:01:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from discord.ext import commands
+import discord
+import requests
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("=========================")
+    print("Bot is ready")
+    print(bot.user.id)
+    print("=========================")
+
+@bot.command()
+async def change(ctx):
+    
+
+bot.run(TOKEN)
+
